57808fce651f67f28485b27c778bd11eb1ad90b5
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-05 18:08:18 +0200

    constants

--- src/js/data/API/constants.js
+++ src/js/data/API/constants.js
@@ -3,3 +3,5 @@ export const API_BASE = "https://v2.api.noroff.dev";
 export const API_AUTH = "/auth";
 export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
+export const API_AUCTION = "/auction";
+export const API_LISTINGS = "/listings";
