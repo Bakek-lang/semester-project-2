3bb73f2a4a4df0413ad759065d94e39952577cce
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-05 15:21:02 +0200

    api key

--- src/js/data/API/constants.js
+++ src/js/data/API/constants.js
@@ -1,1 +1,5 @@
-const API_BASE = "https://v2.api.noroff.dev/";
+export const API_KEY = "8287bbe9-0372-4f79-a466-5889bacee771";
+export const API_BASE = "https://v2.api.noroff.dev";
+export const API_AUTH = "/auth";
+export const API_REGISTER = "/register";
+export const API_LOGIN = "/login";
