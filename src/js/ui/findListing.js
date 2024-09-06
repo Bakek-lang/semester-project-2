a13d2ba936e74b3d9677437d4c93048961f057e3
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-06 09:42:05 +0200

    find id

---
+++ src/js/ui/findListing.js
@@ -0,0 +1,11 @@
+export function findCorrectListing(data) {
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+
+  const matchingListing = data.find(function (object) {
+    return object.id.toString() === id;
+  });
+  console.log(matchingListing);
+  return matchingListing;
+}
