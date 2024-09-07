ab7c623e2972902765f9abed7509cbabc2f3a2f3
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-07 15:02:35 +0200

    profile link cards

--- src/js/eventListeners/sellerLink.js
+++ src/js/eventListeners/sellerLink.js
@@ -1,5 +1,34 @@
+import {
+  API_AUCTION,
+  API_BASE,
+  API_KEY,
+  API_PROFILES,
+} from "../data/API/constants";
+import { fetchData } from "../data/API/fetch.js";
+import { save } from "../localstorage/save";
+import { load } from "../localstorage/load.js";
 export function addSellerLinkClickListener(sellerLink) {
-  sellerLink.addEventListener("click", (event) => {
+  sellerLink.addEventListener("click", async (event) => {
     event.stopPropagation();
+
+    const sellerName = sellerLink.textContent;
+    console.log(sellerName);
+
+    try {
+      const sellerProfile = await fetchData(
+        API_BASE + API_AUCTION + API_PROFILES + "/" + sellerName,
+        {
+          headers: {
+            Authorization: `Bearer ${load("accessToken")}`,
+            "X-Noroff-API-Key": API_KEY,
+          },
+        }
+      );
+
+      console.log(sellerProfile);
+      save("sellerProfile", sellerProfile);
+    } catch (error) {
+      console.error("finding profile didnt work");
+    }
   });
 }
