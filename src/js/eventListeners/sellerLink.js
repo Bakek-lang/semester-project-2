06e9ce50deed314e468bb4ef76f9c06cad66518e
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-07 20:45:13 +0200

    update url

--- src/js/eventListeners/sellerLink.js
+++ src/js/eventListeners/sellerLink.js
@@ -2,11 +2,13 @@ import {
   API_AUCTION,
   API_BASE,
   API_KEY,
+  API_LISTINGS,
   API_PROFILES,
 } from "../data/API/constants";
 import { fetchData } from "../data/API/fetch.js";
 import { save } from "../localstorage/save";
 import { load } from "../localstorage/load.js";
+import { listings } from "../data/API/getListings";
 export function addSellerLinkClickListener(sellerLink) {
   sellerLink.addEventListener("click", async (event) => {
     event.stopPropagation();
@@ -15,8 +17,18 @@ export function addSellerLinkClickListener(sellerLink) {
     console.log(sellerName);
 
     try {
+      console.log(
+        API_BASE + API_AUCTION + API_PROFILES + sellerName + API_LISTINGS
+      );
       const sellerProfile = await fetchData(
-        API_BASE + API_AUCTION + API_PROFILES + "/" + sellerName,
+        // API_BASE + API_AUCTION + API_PROFILES + "/" + sellerName,
+        API_BASE +
+          API_AUCTION +
+          API_PROFILES +
+          "/" +
+          sellerName +
+          API_LISTINGS +
+          "?_bids=true&_seller=true",
         {
           headers: {
             Authorization: `Bearer ${load("accessToken")}`,
