b5cb176ddcec6a0f85643d7b1f83bd9a783b9897
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-08 20:26:20 +0200

    testing

--- src/js/eventListeners/hamburgerMenu.js
+++ src/js/eventListeners/hamburgerMenu.js
@@ -1,10 +1,10 @@
-export function hamburgerMenuToggle() {
-  const menuButton = document.querySelector(
-    "[data-collapse-toggle='navbar-user']"
-  );
-  const menu = document.getElementById("navbar-user");
+// export function hamburgerMenuToggle() {
+//   const menuButton = document.querySelector(
+//     "[data-collapse-toggle='navbar-user']"
+//   );
+//   const menu = document.getElementById("navbar-user");
 
-  menuButton.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-  });
-}
+//   menuButton.addEventListener("click", function () {
+//     menu.classList.toggle("hidden");
+//   });
+// }
