314e851c741831a881f43286cdab5c1a8e289b29
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-08 21:38:27 +0200

    new path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/*/.{js,ts,jsx,tsx,mjs,cjs}", "./**/*.html"],
+  content: [
+    "./index.html",
+    "./src/*/.{js,ts,jsx,tsx,mjs,cjs}",
+    "./**/*.html",
+    ".src/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
