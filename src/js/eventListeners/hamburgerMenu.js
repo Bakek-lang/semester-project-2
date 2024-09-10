a6e42263eac6d31c4b174b162431b1518a5c168e
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-10 13:06:33 +0200

    template

--- src/js/eventListeners/hamburgerMenu.js
+++ src/js/eventListeners/hamburgerMenu.js
@@ -3,7 +3,6 @@ export function hamburgerMenuToggle() {
     "[data-dropdown-toggle='profile-dropdown']"
   );
   const menu = document.getElementById("profile-dropdown");
-  const icon = menuButton.querySelector("i");
 
   menuButton.addEventListener("click", function () {
     menu.classList.toggle("hidden");
