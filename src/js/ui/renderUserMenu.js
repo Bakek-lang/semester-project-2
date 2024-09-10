0d8e06ac8329572b88eb8c66bc22cf86145bee64
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-10 18:28:53 +0200

    fixed selectors

--- src/js/ui/renderUserMenu.js
+++ src/js/ui/renderUserMenu.js
@@ -1,22 +1,28 @@
 export function renderUserMenu(profile) {
-  const profileImage = document.querySelector("data-profile='user-image'");
+  console.log("renderMenu");
+  console.log("profile", profile);
+  const profileImage = document.querySelector("[data-profile='user-image']");
 
   profileImage.src = profile.avatar.url;
   profileImage.alt = profile.avatar.alt;
 
-  const profileName = document.querySelector("data-profile='user-name'");
+  console.log("no problem");
+
+  const profileName = document.querySelector("[data-profile='user-name']");
 
   profileName.textContent = profile.name;
 
-  const profileEmail = document.querySelector("data-profile='user-email'");
+  const profileEmail = document.querySelector("[data-profile='user-email']");
 
   profileEmail.textContent = profile.email;
 
-  const profileCredits = document.querySelector("data-profile='user-credits'");
+  const profileCredits = document.querySelector(
+    "[data-profile='user-credits']"
+  );
 
   profileCredits.textContent = profile.credits;
 
-  const profileWins = document.querySelector("data-profile='user-wins'");
+  const profileWins = document.querySelector("[data-profile='user-wins']");
 
   profileWins.textContent = profile._count.wins;
 }
