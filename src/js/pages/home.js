1298d816274c0be8f5e9e721e6f6a998449170f7
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-16 14:32:12 +0200

    modal

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,8 +1,10 @@
 import { listings } from "../data/API/getListings";
 import { updateUserProfile } from "../data/API/updateUserProfile";
+import { modalListeners } from "../eventListeners/modalListeners";
 import { createCards } from "../ui/createCard.js";
 
 export function homePage() {
+  modalListeners();
   const container = document.querySelector("[data-attribute='container'");
   console.log(listings);
   createCards(listings, container);
