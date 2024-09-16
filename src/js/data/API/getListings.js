af54aba922e12481e9e4386fdf9b17c171e2c382
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-16 21:13:21 +0200

    sorting complete

--- src/js/data/API/getListings.js
+++ src/js/data/API/getListings.js
@@ -29,6 +29,23 @@ async function fetchAllListings() {
   }
 
   const sortedListings = allListings.sort((a, b) => {
+    const now = new Date();
+
+    const isAExpired = new Date(a.endsAt) < now;
+    const isBExpired = new Date(b.endsAt) < now;
+
+    if (!isAExpired && !isBExpired) {
+      return new Date(a.endsAt) - new Date(b.endsAt);
+    }
+
+    if (isAExpired && !isBExpired) {
+      return 1;
+    }
+
+    if (!isAExpired && isBExpired) {
+      return -1;
+    }
+
     return new Date(a.endsAt) - new Date(b.endsAt);
   });
 
