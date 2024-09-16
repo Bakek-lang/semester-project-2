b300ebbccb108f115482e6074344c1bcd0c5bf69
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-16 20:52:15 +0200

    all listings and sort

--- src/js/data/API/getListings.js
+++ src/js/data/API/getListings.js
@@ -1,6 +1,40 @@
 import { API_AUCTION, API_BASE, API_LISTINGS } from "./constants.js";
 import { fetchData } from "./fetch.js";
 
-export const listings = await fetchData(
-  `${API_BASE}${API_AUCTION}${API_LISTINGS}?_bids=true&_seller=true`
-);
+// export const listings = await fetchData(
+//   `${API_BASE}${API_AUCTION}${API_LISTINGS}?_bids=true&_seller=true`
+// );
+
+async function fetchAllListings() {
+  let allListings = [];
+  let currentPage = 1;
+  let totalPageCount = 1;
+  const LIMIT = 100;
+
+  const firstResponse = await fetchData(
+    `${API_BASE}${API_AUCTION}${API_LISTINGS}?_bids=true&_seller=true&page=${currentPage}&limit=${LIMIT}`
+  );
+  allListings = allListings.concat(firstResponse.data);
+  console.log("All listings after first response: ", allListings);
+  totalPageCount = firstResponse.meta.pageCount;
+
+  while (currentPage < totalPageCount) {
+    currentPage++;
+    const nextResponse = await fetchData(
+      `${API_BASE}${API_AUCTION}${API_LISTINGS}?_bids=true&_seller=true&page=${currentPage}&limit=${LIMIT}`
+    );
+
+    allListings = allListings.concat(nextResponse.data);
+    console.log("All Listings in while loop: ", allListings);
+  }
+
+  const sortedListings = allListings.sort((a, b) => {
+    return new Date(a.endsAt) - new Date(b.endsAt);
+  });
+
+  console.log("sorted listings: ", sortedListings);
+
+  return sortedListings;
+}
+
+export const listings = await fetchAllListings();
