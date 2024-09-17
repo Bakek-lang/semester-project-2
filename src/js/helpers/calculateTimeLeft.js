13ceb05da7a7656eed5df5cd7ba2d9522a5be774
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-17 18:06:33 +0200

    timeLeft update

--- src/js/helpers/calculateTimeLeft.js
+++ src/js/helpers/calculateTimeLeft.js
@@ -3,6 +3,7 @@ export function calculateTimeLeft(endsAt) {
   const endDate = new Date(endsAt);
   const diff = endDate - now;
 
+  const secondsLeft = Math.floor(diff / 1000);
   const minutesLeft = Math.floor(diff / (1000 * 60));
   const hoursLeft = Math.floor(diff / (1000 * 60 * 60));
   const daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
@@ -13,6 +14,8 @@ export function calculateTimeLeft(endsAt) {
     return `${hoursLeft} hour${hoursLeft > 1 ? "s" : ""} left`;
   } else if (minutesLeft > 0) {
     return `${minutesLeft} minute${minutesLeft > 1 ? "s" : ""} left`;
+  } else if (secondsLeft > 0) {
+    return `< 1 minute left`;
   } else {
     return `Expired`;
   }
