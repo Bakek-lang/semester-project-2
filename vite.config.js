b9120fc23f352889e9a64ec0613c49c986c2df57
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-20 20:19:09 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,16 @@ export default {
   esbuild: {
     target: "esnext",
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        post: "./post/index.html",
+        profile: "./profile/index.html",
+        settings: "./settings/index.html",
+        login: "./login/index.html",
+        register: "./register/index.html",
+      },
+    },
+  },
 };
