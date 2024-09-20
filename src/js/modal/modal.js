3dc610ac8f3f72d93e1bed16d4a0e305415dc92e
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-09-20 11:08:49 +0200

    styling

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,11 +1,13 @@
 export function openModal() {
   const modal = document.getElementById("modal-overlay");
   modal.classList.remove("hidden");
+  modal.classList.add("flex");
   document.body.classList.add("overflow-hidden");
 }
 
 export function closeModal() {
   const modal = document.getElementById("modal-overlay");
+  modal.classList.remove("flex");
   modal.classList.add("hidden");
   document.body.classList.remove("overflow-hidden");
 }
